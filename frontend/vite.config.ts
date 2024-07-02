36a8018249066867317d52d52d500cd1962db0c7
Gingarenpo <gingarenpo.9844@gmail.com>
2024-07-02 10:08:42 +0900

    Setup package.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  // GitHubによるBuildの場合、ベースディレクトリを変更する
+  base: process.env.GITHUB_PAGES ? "Gingarenpo" : "./",
 })
